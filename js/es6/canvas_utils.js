6bae9e4077ef42fb88376b59d3c379d0ddee15d8
Eric Pai <ericpai@berkeley.edu>
2015-03-22 20:28:56 -0400

    something

--- js/es6/canvas_utils.js
+++ js/es6/canvas_utils.js
@@ -24,10 +24,17 @@ export class CanvasUtils {
 	}
 
 	static drawLine(ctx, coordFrom, coordTo, lineWidth, directed, stroke=true, color="black") {
+		var midPoint = [
+			coordFrom[0] + (coordTo[0] - coordFrom[0]) / 2,
+			coordFrom[1] + (coordTo[1] - coordFrom[1]) / 2
+		]
+
 		ctx.beginPath();
 		ctx.moveTo(coordFrom[0], coordFrom[1]);
-		// ctx.lineTo(coordTo[0], coordTo[1]);
-		ctx.quadraticCurveTo(coordFrom[0], coordFrom[1], coordTo[0], coordTo[1]);
+		ctx.lineCap = 'round';
+		ctx.lineTo(coordTo[0], coordTo[1]);
+		// ctx.quadraticCurveTo(coordFrom[0], coordFrom[1], midPoint[0], midPoint[1]);
+		// ctx.quadraticCurveTo(midPoint[0], midPoint[1], coordTo[0], coordTo[1]);
 		// Need to add code that draws an arrow when directed = True
 		ctx.strokeStyle = color;
 		ctx.lineWidth = lineWidth;
@@ -38,8 +45,8 @@ export class CanvasUtils {
 		else {
 			ctx.closePath();
 		}
-		ctx.stroke();
-		console.log("stroke")
+		// ctx.stroke();
+		// console.log("stroke")
 	}
 
 }
